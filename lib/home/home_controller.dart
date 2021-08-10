48800a9876d1ce08f44b7b9b684c25090648907c
Gabul <contato@gabul.dev>
2021-08-10 19:38:08 -0300

    first commit

---
+++ lib/home/home_controller.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:fpflutter/home/home_repository.dart';
+import 'package:fpflutter/home/home_state.dart';
+
+class HomeController {
+  final HomeRepository repository;
+
+  final stateNotifier = ValueNotifier<HomeState>(HomeStateEmpty());
+  HomeState get state => stateNotifier.value;
+  set state(HomeState state) => stateNotifier.value = state;
+
+  HomeController({
+    required this.repository,
+  });
+
+  Future<void> getNames() async {
+    final response = await this.repository.getNames();
+    state = response.fold(
+      (l) => HomeStateError(l),
+      (r) => HomeStateSuccess(r),
+    );
+  }
+}
